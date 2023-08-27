6d7bcb5423c93135d4b4570c1c5f330a73f61a2f
toqafotoh <100733611+toqafotoh@users.noreply.github.com>
2023-08-27 04:06:31 +0300

    Update DataCamp_practise.py

--- DataCamp_practise.py
+++ DataCamp_practise.py
@@ -171,3 +171,34 @@ mse_1 = mean_squared_error(target_actuals,model_output_1)
 # Print mse_0 and mse_1
 print("Mean squared error with weights_0: %f" %mse_0)
 print("Mean squared error with weights_1: %f" %mse_1)
+
+
+#__________________________________________________________
+
+
+# Set the learning rate: learning_rate
+learning_rate = 0.01
+
+# Calculate the predictions: preds
+preds = (weights * input_data).sum()
+
+# Calculate the error: error
+error = preds - target
+
+# Calculate the slope: slope
+slope = 2 * input_data * error
+
+# Update the weights: weights_updated
+weights_updated = weights - (learning_rate*slope)
+
+# Get updated predictions: preds_updated
+preds_updated = (weights_updated* input_data).sum()
+
+# Calculate updated error: error_updated
+error_updated = preds_updated - target
+
+# Print the original error
+print(error)
+
+# Print the updated error
+print(error_updated)
