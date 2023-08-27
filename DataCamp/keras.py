05241c19e3387a07adc2b1c31a6d0ace5d7b1fdd
toqafotoh <100733611+toqafotoh@users.noreply.github.com>
2023-08-27 14:35:23 +0300

    Update keras.py

--- DataCamp/keras.py
+++ DataCamp/keras.py
@@ -17,3 +17,23 @@ model.add(Dense(32, activation='relu'))
 
 # Add the output layer
 model.add(Dense(1))
+
+
+#______________________________________________________
+
+# Import necessary modules
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.models import Sequential
+
+# Specify the model
+n_cols = predictors.shape[1]
+model = Sequential()
+model.add(Dense(50, activation='relu', input_shape = (n_cols,)))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(1))
+
+# Compile the model
+model.compile(loss='mean_squared_error',optimizer='adam')
+
+# Verify that model contains information from compiling
+print("Loss function: " + model.loss)
