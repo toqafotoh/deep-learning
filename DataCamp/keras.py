b9569d0871a1db3de9d117b6b493be33edb1b8d5
toqafotoh <100733611+toqafotoh@users.noreply.github.com>
2023-08-27 16:38:58 +0300

    Update keras.py

--- DataCamp/keras.py
+++ DataCamp/keras.py
@@ -37,3 +37,53 @@ model.compile(loss='mean_squared_error',optimizer='adam')
 model.fit(predictors,target)
 # Verify that model contains information from compiling
 print("Loss function: " + model.loss)
+
+
+
+#________________________________________
+
+# A Code for classification
+# Import necessary modules
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.utils import to_categorical
+
+# Convert the target to categorical: target
+target = to_categorical(df.survived)
+# Set up the model
+model = Sequential()
+
+# Add the first layer
+model.add(Dense(32,activation='relu',input_shape=(n_cols,)))
+# Add the output layer
+model.add(Dense(2, activation='softmax'))
+
+
+# Compile the model
+model.compile(loss='categorical_crossentropy',optimizer='sgd',metrics=['accuracy'])
+
+# Fit the model
+model.fit(predictors, target)
+
+
+#_____________________________________
+# Specify, compile, and fit the model
+model = Sequential()
+model.add(Dense(32, activation='relu', input_shape = (n_cols,)))
+model.add(Dense(2, activation='softmax'))
+model.compile(optimizer='sgd', 
+              loss='categorical_crossentropy', 
+              metrics=['accuracy'])
+model.fit(predictors, target)
+
+# Calculate predictions: predictions
+predictions = model.predict(pred_data)
+
+# Calculate predicted probability of survival: predicted_prob_true
+predicted_prob_true = predictions[:,1]
+
+# Print predicted_prob_true
+print(predicted_prob_true)
+
+#_____________________________________________________
+
