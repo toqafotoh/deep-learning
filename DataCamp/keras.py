69c20dd91f2e813c33151add5d9bc4584d4f52ba
toqafotoh <100733611+toqafotoh@users.noreply.github.com>
2023-09-02 21:14:42 +0300

    Update keras.py

--- DataCamp/keras.py
+++ DataCamp/keras.py
@@ -87,3 +87,25 @@ print(predicted_prob_true)
 
 #_____________________________________________________
 
+# Import the SGD optimizer
+from tensorflow.keras.optimizers import SGD
+
+# Create list of learning rates: lr_to_test
+lr_to_test = [.000001, 0.01,1]
+
+# Loop over learning rates
+for lr in lr_to_test:
+    print('\n\nTesting model with learning rate: %f\n'%lr )
+    
+    # Build new model to test, unaffected by previous models
+    model = get_new_model()
+    
+    # Create SGD optimizer with specified learning rate: my_optimizer
+    my_optimizer = SGD(lr=lr)
+    
+    # Compile the model
+    model.compile(loss='categorical_crossentropy', optimizer=my_optimizer)
+    
+    # Fit the model
+    model.fit(predictors,target)
+
